edc5cfc8d077883770e0fd9919d5388b82907164
Shinmo <shamsitabar.mohsen@gmail.com>
2025-06-12 11:23:12 +0330

    config project (#1)

---
+++ constants/network.ts
@@ -0,0 +1,5 @@
+export const SERVER_PORT = 3001;
+export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
+
+export const CLIENT_PORT = 5173;
+export const CLIENT_URL = `http://localhost:${CLIENT_PORT}`;
