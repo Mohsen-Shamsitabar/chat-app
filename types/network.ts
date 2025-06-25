7cb393d6da09a6bba58a14d0dddfd78a8cdce81d
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-06-25 11:29:57 +0330

    add global types

---
+++ types/network.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from "express";
+import type { LoginFormSchema } from "../schemas/login-form.ts";
+
+export type LoginRequest = Request<unknown, unknown, LoginFormSchema>;
+
+export type LoginResponseBody = {
+  message: string;
+};
+
+export type LoginResponse = Response<LoginResponseBody>;
