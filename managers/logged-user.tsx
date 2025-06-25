f66c48777dacdd7681245afd7813ff6a358b394b
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-06-25 11:29:32 +0330

    add global state manager

---
+++ managers/logged-user.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { type LoginFormSchema } from "../schemas/login-form.ts";
+
+type LoggedUserManager = {
+  loggedUser: LoginFormSchema | null;
+  setLoggedUser: React.Dispatch<
+    React.SetStateAction<{
+      username: string;
+    } | null>
+  >;
+};
+
+const Context = React.createContext<LoggedUserManager | null>(null);
+
+type ProviderProps = React.PropsWithChildren<LoggedUserManager>;
+
+export const LoggedUserManagerProvider = (props: ProviderProps) => {
+  const { children, ...data } = props;
+
+  return <Context.Provider value={data}>{children}</Context.Provider>;
+};
+
+export const useLoggedUserManager = () => React.useContext(Context);
