7e4400ad54d2fa55f1c2d9c1c5bbfbec7ecfd145
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-06-25 11:29:06 +0330

    add global constants

--- constants/network.ts
+++ constants/network.ts
@@ -1,5 +1,26 @@
 export const SERVER_PORT = 3001;
+// export const SERVER_URL = `http://192.168.1.144:${SERVER_PORT}`;
 export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
 
 export const CLIENT_PORT = 5173;
+// export const CLIENT_URL = `http://192.168.1.144:${CLIENT_PORT}`;
 export const CLIENT_URL = `http://localhost:${CLIENT_PORT}`;
+
+export const API_ROUTES = {
+  LOGIN: "/users/login",
+} as const;
+
+export const CLIENT_ROUTES = {
+  LOGIN: "/",
+  CHATROOM: "/chatroom",
+} as const;
+
+export const CHANNELS = {
+  LOGIN: "users/login",
+  SUBMIT_MESSAGE: "messages/submit",
+  NOTIFY_MESSAGE: "messages/notify",
+
+  // built in
+  DISCONNECT: "disconnect",
+  CONNECT: "connect",
+} as const;
